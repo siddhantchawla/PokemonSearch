c9d4d7211143972aea6793f1869116c42dddaba8
siddhant <siddhant.chawla9@gmail.com>
2020-05-17 01:51:19 +0530

    first commit

---
+++ src/components/PokemonSearch.tsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import User from '../interfaces/User.interface'
+
+interface SearchState{
+    error : boolean,
+    pokemon : Pokemon
+}
+
+interface Pokemon{
+    name : string,
+    noOfAbilities : number,
+    baseExperience : number,
+    imageUrl : string
+}
+
+export class PokemonSearch extends Component<User,SearchState> {
+    pokemonRef: React.RefObject<HTMLInputElement>
+
+    constructor(props : User) {
+        super(props);
+        this.state = {
+            error : false,
+            pokemon : null
+        }
+        this.pokemonRef = React.createRef();
+    }
+
+    onSearchClick = () => {
+        const inputValue = this.pokemonRef.current.value;
+        fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`)
+            .then(res => {
+                if(res.status !== 200)
+                {
+                    this.setState({
+                        error : true
+                    });
+                    return;
+                }
+                else{
+                    res.json().then(data => {
+                        this.setState(
+                            {
+                                error : false,
+                                pokemon : {
+                                    name: data.name,
+                                    noOfAbilities: data.abilities.length,
+                                    baseExperience: data.base_experience,
+                                    imageUrl: data.sprites.front_default
+                                }
+                            }
+                        );
+                    });
+                }
+            });
+
+    }
+
+    render() {
+        const { name: Username, numberOfPokemons } = this.props;
+        const { error, pokemon} = this.state;
+
+        let resultMarkup;
+
+        if(error)
+        {
+            resultMarkup = <p> Pokemon not found. Please Try again. </p>
+        }
+        else if(pokemon){
+            resultMarkup = <div>
+                <img src={pokemon.imageUrl} alt="pokemon" className="pokemon-image" />
+                <p>
+                    {pokemon.name} has {pokemon.noOfAbilities} abilities and {pokemon.baseExperience} base experience points.
+                </p>
+            </div>
+        }
+
+        return (
+            <div>
+                <p>User {Username} {' '}
+                {numberOfPokemons && <span>has {numberOfPokemons} pokemons.</span>}
+                </p>
+                <input type='text' ref={this.pokemonRef} />
+                <button onClick={this.onSearchClick} className='my-button'>
+                    Search
+                </button>
+                {resultMarkup}
+            </div>
+        )
+    }
+}
+
+export default PokemonSearch
